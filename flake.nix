8a684598d18b53999dfc0af48053146938fc5872
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-05-26 14:09:02 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,12 +13,21 @@
     n2c = nix2container.outputs.packages.${system}.nix2container;
     version = "12.10.2";
     srcHash = "sha256-2PQPs7aIHd2h6bBiyQtFh+5afaI2uAq5mhx//xtifWE=";
+
+    containerPackages = [
+      pkgs.nix
+      pkgs.podman
+    ];
+
     imageConfig = {
       Volumes = {
         "/data" = {};
       };
       Env = [
         "HOME=/data"
+        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+        "NIX_CONF_DIR=/data/nix"
+        "NIX_STATE_DIR=/data/var/nix"
       ];
       WorkingDir = "/data";
       Cmd = [ "${pkgs.forgejo-runner}/bin/forgejo-runner" "daemon" "--config" "/data/config.yaml" ];
@@ -26,14 +35,6 @@
   in {
     packages.${system} = {
       forgejo-runner-image = n2c.buildImage {
-        name = "forgejo-runner";
-        tag = "latest";
-        fromImage = base.packages.${system}.base-image;
-        maxLayers = 5;
-        config = imageConfig;
-      };
-
-      forgejo-runner-debug-image = n2c.buildImage {
         name = "forgejo-runner";
         tag = "latest-debug";
         fromImage = base.packages.${system}.base-debug-image;
